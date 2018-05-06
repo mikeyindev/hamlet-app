ebe1f3aa3a7ca862933ddbd6b7092024f8ee96e3
mikeyindev <mikeyindev@gmail.com>
2018-05-06 13:50:31 -0400

    Styled react-modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,9 +5,12 @@ Modal.setAppElement(document.getElementById("app"));
 
 const OptionModal = (props) => (
   // onRequestClose allows users to click outside the modal or hit the esc button to close the modal
-  <Modal isOpen={!!props.selectedOption} onRequestClose={props.clearSelectedOption}>
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
+  <Modal isOpen={!!props.selectedOption} 
+        onRequestClose={props.clearSelectedOption}
+        closeTimeoutMS={300}
+        className="modal">
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
     <button onClick={props.clearSelectedOption}>Ok</button>
   </Modal>
 );
