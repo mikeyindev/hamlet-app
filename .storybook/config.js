977b429fff49d871c982b567e9a194130254bcc6
mikeyindev <mikeyindev@gmail.com>
2018-05-15 19:46:47 -0400

    Changed Storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,9 +2,22 @@ import { configure } from '@storybook/react';
 import { setOptions } from "@storybook/addon-options";
 import '../src/styles/styles.scss';
 
-// Change name of Storybook to 'Hamlet App'
 setOptions({
   name: 'Hamlet App'
 });
 
-configure(() => require('./stories'), module);
+const req = require.context("../src", true, /.stories.js$/);
+
+const loadStories = () => {
+  // require('../stories/Action.stories.js');
+  // require('../stories/AddOption.stories.js');
+  // require('../stories/HamletApp.stories.js');
+  // require('../stories/Header.stories.js');
+  // require('../stories/Option.stories.js');
+  // require('../stories/OptionModal.stories.js');
+  // require('../stories/Options.stories.js');
+
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
