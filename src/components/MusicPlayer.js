1332c2f8f6e490b298676bf1162dc9ecc47fe28b
mikeyindev <mikeyindev@gmail.com>
2018-06-16 11:10:44 -0500

    Moved audio tag to component from index.html

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -3,14 +3,12 @@ import React from 'react';
 const MusicPlayer = (props) => {
   const buttonText = props.isPlaying ? 'Pause music' : 'Play music!';
 
-  return (
-    <div className="music-player">
-      <button 
-        className="button--link button__play" 
-        onClick={props.handlePlayMusic}
-      >{buttonText}</button>
-    </div>
-  );
+  return <div className="music-player">
+      <audio id="music" src="/music/Jahzzar_-_05_-_Siesta.mp3" />
+      <button className="button--link button__play" onClick={props.handlePlayMusic}>
+        {buttonText}
+      </button>
+    </div>;
 };
 
 export default MusicPlayer;
