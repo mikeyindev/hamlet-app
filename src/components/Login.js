d238a18cdfe8ec36eb39951d675d33d4de0436fd
mikeyindev <mikeyindev@gmail.com>
2018-06-22 18:30:19 -0500

    Changed the app's color scheme

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Login = (props) => (
   <div>
     {!props.isLoggedIn ? 
-      <button className="button__link button--menu button--login" onClick={props.handleLogin}>Login
+      <button className="button--menu button--login" onClick={props.handleLogin}>Login
       </button> :
-      <button className="button__link button--menu button--logout" onClick={props.handleLogout}>
+      <button className="button--menu button--logout" onClick={props.handleLogout}>
         Logout
       </button>}
   </div>
