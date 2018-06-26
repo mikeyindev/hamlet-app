852a5872e01f55002825f2519cec3c4991da295b
mikeyindev <mikeyindev@gmail.com>
2018-06-26 12:41:29 -0500

    Refactored styles

--- src/components/Action.js
+++ src/components/Action.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Action = (props) => {
   return <div>
-      <button className="big-button" onClick={props.handlePick} disabled={!props.hasOptions}>
+      <button className="button big-button" onClick={props.handlePick} disabled={!props.hasOptions}>
         What should I do?
       </button>
     </div>;
